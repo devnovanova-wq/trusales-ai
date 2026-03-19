8b1aaa29f00447c0f0389a7e657efa63a42606c7
megastorecoordinacion <megastore.coordinacion@futuravive.com>
2026-03-19 12:59:49 +0100

    meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { DM_Sans, Inter, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -24,7 +25,7 @@ import Providers from "@/components/Providers";
 
 export const metadata = {
   title: "Tru Sales",
-  description: "Never lose a sales lead again",
+  description: "Nunca vuelvas a perder un Lead",
 };
 
 export default function RootLayout({
@@ -35,6 +36,34 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${dmSans.variable} ${inter.variable} ${spaceGrotesk.variable}`}>
+        
+        {/* Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '924752757151274');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=924752757151274&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Providers>{children}</Providers>
       </body>
     </html>
