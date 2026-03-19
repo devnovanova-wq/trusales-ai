b797ade963df22fd72d85deda87a0c5e96e2edee
megastorecoordinacion <megastore.coordinacion@futuravive.com>
2026-03-19 22:40:59 +0100

    script layour clatiry

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,17 +52,13 @@ export default function RootLayout({
             fbq('init', '924752757151274');
             fbq('track', 'PageView');
           `}
-        </Script>
+        </Script>,
+import Script from "next/script";
 
-        <Script id="clarity" strategy="afterInteractive">
-  {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "vyb43zto4t");
-  `}
-</Script>
+<Script
+  src="https://www.clarity.ms/tag/vyb43zto4t"
+  strategy="afterInteractive"
+/>
 
         {/* Noscript fallback (Meta) */}
         <noscript>
