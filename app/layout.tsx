e405d1314114f3d2008a1a348788b5bfe73d4f0d
megastorecoordinacion <megastore.coordinacion@futuravive.com>
2026-03-19 22:54:23 +0100

    clarity html

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,9 +56,18 @@ export default function RootLayout({
 
         {/* Microsoft Clarity */}
         <Script
-          src="https://www.clarity.ms/tag/vyb43zto4t"
-          strategy="afterInteractive"
-        />
+  id="clarity"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "vyb43zto4t");
+    `,
+  }}
+/>
 
         {/* Noscript fallback */}
         <noscript>
