ffb5af873fcc98142f927aa4d8188ce23ad9d2d4
megastorecoordinacion <megastore.coordinacion@futuravive.com>
2026-03-19 22:36:39 +0100

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,19 +54,15 @@ export default function RootLayout({
           `}
         </Script>
 
-        <Script
-  id="clarity"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "vyb43zto4t");
-    `,
-  }}
-/>
+        <Script id="clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vyb43zto4t");
+  `}
+</Script>
 
         {/* Noscript fallback (Meta) */}
         <noscript>
